1adad30e005c76011dfa6ca5f6c9db4b37a0016e
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-07-02 21:15:31 +0100

    initial stab adding first cell populated to board and all its descendants
    the first cell populated in the tree of all future boards is called "cell" and follows the board and all of its descendants throughout future populations

--- scripts/allBoards.js
+++ scripts/allBoards.js
@@ -11,7 +11,7 @@ var winningBoardStates = [];
 function allFutureBoards(boards, move){
   var newBoards = [];
   for(var i = 0; i < boards.length; i++){
-    if(gameHasWinner(boards[i], move)){
+    if(gameHasWinner(boards[i].board, move)){
       newBoards.push(boards[i]);
     } else {
       newBoards.push(populate(boards[i], move));
@@ -26,34 +26,37 @@ function allFutureBoards(boards, move){
 
 
 // takes a single board and returns an array of all the possible boards after the next move
-function populate(board, move){
+function populate(boardState, move){
   if (move === testMove) {
     console.log("at the top of the tree");
+    boardState.cell = testMove; //set initial value of cell, will follow the board and all of its descendants throughout future populations
     // must find a way to store emptySpaces[i] on each new board pushed into temp
     // and then also store emptySpaces[i] at the top of the tree on each of its descendants
     // adding to temp object will break flattenedAndUnique functionality
     // might store each branch in its own array?
-  }
+  };
   var temp = [];
   var emptySpaces = [];
-  var id = board.indexOf(null);
+  var id = boardState.board.indexOf(null);
   while (id != -1) {
     emptySpaces.push(id);
-    id = board.indexOf(null, id + 1);
+    id = boardState.board.indexOf(null, id + 1);
   }
   for(var i = 0; i < emptySpaces.length; i++){ // emptySpaces[i] is the board's last move, but we actually need the number of the cell at the top of the "tree"
-    var copy = board.slice(0, 9);
+    var copy = boardState.board.slice(0, 9);
     if (move%2 === 0) copy[emptySpaces[i]]=1;
     else copy[emptySpaces[i]]=-1;
     if (gameHasWinner(copy, move) && isNewWinningState(copy)){
 
-      winningBoardStates.push({board: copy, lastMoveCell: emptySpaces[i]});
+      winningBoardStates.push({board: copy, cell: boardState.cell});
+
     }
-    temp.push(copy);
+    temp.push({board: copy, cell: boardState.cell});
   }
   return temp;
 }
 
+// avoid hard-coding 9
 function isNewWinningState(board){
   var isUnique = true;
   for (var i=0; i<winningBoardStates.length; i++){
@@ -68,25 +71,30 @@ function isNewWinningState(board){
   return isUnique;
 }
 
-function flattenedAndUnique(array) {
-    var singleBoard = [];
-    var arrayOfBoards = [];
-    for (var i=0; i<array.length; i++){
-      if (array[i].length === 9 && array[i][0] === null || array[i][0].length !== 9) {
-        singleBoard.push(array[i]);
-      } else arrayOfBoards.push(array[i]);
-    }
-    var tempflattened = [].concat.apply([], arrayOfBoards);
-    var allBoards = singleBoard.concat(tempflattened);
 
-    var hash = {}, unique = [];
-    for ( var i=0; i<allBoards.length; ++i ) {
-        if ( !hash.hasOwnProperty(allBoards[i]) ) { 
-            hash[ allBoards[i] ] = true;
-            unique.push(allBoards[i]);
-        }
+// flatten, but only so much
+function flattenedAndUnique(array) {
+  var singleBoard = [];
+  var arrayOfBoards = [];
+  for (var i=0; i<array.length; i++){
+    // debugger;
+    if (array[0].length === undefined) { // if it's just a single board object rather than array of objects
+      singleBoard.push(array[i]);
+    } else arrayOfBoards.push(array[i]);
+  }
+  var tempflattened = [].concat.apply([], arrayOfBoards);
+  var allBoards = singleBoard.concat(tempflattened);
+
+  var hash = {}; 
+  var unique = [];
+  for(var i=0; i<allBoards.length; i++){
+    if(!hash.hasOwnProperty(allBoards[i])){ 
+      // debugger;
+      hash[ allBoards[i] ] = true;
+      unique.push(allBoards[i]);
     }
-    return unique;
+  }
+  return unique;
 }
 
 
@@ -102,7 +110,7 @@ function gameHasWinner(board, move) {
 
 // var testBoard = [[ null, null, null, null, null, null, null, null, null ]];
 // var testMove = 0;
-var testBoard = [[ -1, null, 1, 1, null, null, 1, -1, -1 ]]; // returns all 5 potential win states
+var testBoard = [{board: [ -1, null, 1, 1, null, null, 1, -1, -1 ], cell: null}]; // returns all 5 potential win states
 var testMove = 6;
 var testingUniqueness = allFutureBoards(testBoard, testMove);
 console.log(testingUniqueness);
